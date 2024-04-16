e25a97cf003049a17619ae639e930a5705a28182
Harvey Fretwell <47286380+harveyf2801@users.noreply.github.com>
2024-04-16 15:49:59 +0100

    lesson script

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ import plotting
 AUDIO_FILES = Path(os.getcwd(), 'wavfiles')
 ANNOTATIONS = Path(os.getcwd(), 'annotations.csv')
 # Setting wether to run plots or not
-PLOTTING = True
+PLOTTING = False
 
 # Create annotations file if not already created for the dataset
 # or load in the annotations csv file
@@ -50,11 +50,16 @@ SAMPLE_RATE = 22050
 NUM_SAMPLES = 22050
 BATCH_SIZE = 128
 
+WIN_LENGTH = 2**10
+HOP_LENGTH = 2**9
+NMFCC = 13 # 64?
+
 # Creating a mel spectogram for the feature extraction / transformation
 transformation = torchaudio.transforms.MelSpectrogram(
     sample_rate=SAMPLE_RATE,
-    n_fft=1024,
-    hop_length=512,
+    win_length=WIN_LENGTH,
+    n_fft=WIN_LENGTH,
+    hop_length=HOP_LENGTH,
     n_mels=64
 )
 
@@ -66,3 +71,51 @@ logging.info(f"Device available: {device}")
 audio_dataset = dataset.AudioDataset(annotations, AUDIO_FILES, device, transformation, SAMPLE_RATE, NUM_SAMPLES)
 data_loader = DataLoader(audio_dataset, batch_size=BATCH_SIZE)
 
+# def spectral_features(signal):
+#     spectrogram = torch.stft(signal, win_length=WIN_LENGTH, n_fft=WIN_LENGTH, hop_length=HOP_LENGTH)
+#     M = np.abs(spectrogram)
+#     sc = librosa.feature.spectral_centroid(S=M)
+#     sf = librosa.feature.spectral_flatness(S=M)
+
+i = 0
+for input, target in audio_dataset:
+    sf = spectral_features(input)
+    mfcc_mean[i, :] = np.mean(sf['mfccs'][:, 1:], axis=1)
+    sc_mean[i] = np.mean(sf['spectral_centroids'])
+    sf_mean[i] = np.mean(sf['spectral_flatness'])
+    print(target)
+
+    i+=1
+
+# KNN with k-fold cross-validation
+# perform k-fold cross-validation
+
+# perform knn
+n_k = 3
+acc = np.zeros(c.numtestsets, 1)
+pred_labels = []
+true_labels = []
+for k in range(c.numtestsets):
+    train_data = features[c.training[k], :]
+    train_ann = ann[c.training[k]]
+    test_data = features[c.test[k], :]
+    test_ann = ann[c.test[k]]
+    training_std = zscore[train_data]
+    coeff, _, _, _, explain = pca(training_std) # could also use LDA
+
+    var_thresh = 95 # 95 percent
+    cu_var = np.cumsum(explain)
+    n_pc = find[cu_var >= var_thresh]
+    train_data_pca = training_std * coeff[:, 0:n_pc]
+    test_data_pca = (test_data - np.mean(train_data)) / np.std(train_data, axis=0) * coeff[:, 0:n_pc]
+
+    knn_model = fitcknn(train_data_pca, train_ann, NumNeighbors=n_k)
+    pred_knn = predict(knn_model, test_data_pca)
+    acc[k] = np.sum(pred_knn == test_ann) / numel(test_ann)
+
+    pred_labels = pred_labels.append(pred_knn.T)
+    true_labels = true_labels.append(test_ann.T)
+
+score = np.mean(acc)*100
+C = confusionmat(true_labels, pred_labels)
+confusionchart(C, class_labels)
